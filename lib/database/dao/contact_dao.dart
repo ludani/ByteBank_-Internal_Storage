c9bfebaf80cd6df5096b2fa84116ff2b9a1a6a3c
Lucas Vieira <lucasvieira@bemol.onmicrosoft.com>
2020-08-02 18:44:07 -0400

    ajuste no banco de dados

--- lib/database/dao/contact_dao.dart
+++ lib/database/dao/contact_dao.dart
@@ -1,31 +1,44 @@
+import 'package:bankbyte/database/app_database.dart';
 import 'package:bankbyte/models/contact.dart';
 import 'package:sqflite/sqflite.dart';
 
-import '../app_database.dart';
-
 class ContactDao {
-  static const String tableSql = 'CREATE TABLE contacts('
-      'id INTEGER PRIMARY KEY, '
-      'name TEXT, '
-      'account_number INTEGER)';
+  static const String tableSql = 'CREATE TABLE $_tableName('
+      '$_id INTEGER PRIMARY KEY, '
+      '$_name TEXT, '
+      '$_accountNumber INTEGER)';
+  static const String _tableName = 'contacts';
+  static const String _id = 'id';
+  static const String _name = 'name';
+  static const String _accountNumber = 'account_number';
 
   Future<int> save(Contact contact) async {
     final Database db = await createDatabase();
-    final Map<String, dynamic> contactMap = Map();
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
-    return db.insert('contacts', contactMap);
+    Map<String, dynamic> contactMap = _toMap(contact);
+    return db.insert(_tableName, contactMap);
   }
 
   Future<List<Contact>> findAll() async {
     final Database db = await createDatabase();
-    final List<Map<String, dynamic>> result = await db.query('contacts');
+    final List<Map<String, dynamic>> result = await db.query(_tableName);
+    List<Contact> contacts = _toList(result);
+    return contacts;
+  }
+
+  Map<String, dynamic> _toMap(Contact contact) {
+    final Map<String, dynamic> contactMap = Map();
+    contactMap[_name] = contact.name;
+    contactMap[_accountNumber] = contact.accountNumber;
+    return contactMap;
+  }
+
+  List<Contact> _toList(List<Map<String, dynamic>> result) {
     final List<Contact> contacts = List();
     for (Map<String, dynamic> row in result) {
       final Contact contact = Contact(
-        row['id'],
-        row['name'],
-        row['account_number'],
+        row[_id],
+        row[_name],
+        row[_accountNumber],
       );
       contacts.add(contact);
     }
